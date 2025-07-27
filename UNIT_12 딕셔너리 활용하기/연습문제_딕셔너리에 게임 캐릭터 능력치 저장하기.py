5d2bb68da01ea39c268ec2d5fc2953851b249842
yeojin <yeojin3067@gmail.com>
2025-07-27 12:51:50 +0900

    day7

---
+++ UNIT_12 딕셔너리 활용하기/연습문제_딕셔너리에 게임 캐릭터 능력치 저장하기.py
@@ -0,0 +1,17 @@
+# 다음 소스코드를 완성하여 게임 캐릭터의 체력(health)과 이동 속도(movement speed)가 출력되게 만드세요.
+camille = {
+    'health': 575.6,
+    'health_regen': 1.7,
+    'mana': 338.8,
+    'mana_regen': 1.63,
+    'melee': 125,
+    'attack_damage': 60,
+    'attack_speed': 0.625,
+    'armor': 26,
+    'magic_resistance': 32.1,
+    'movement_speed': 340
+}
+
+# 여기서부터 작성
+print(camille['health'])
+print(camille['movement_speed'])
